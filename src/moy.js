a6372676d9b30af3300932429456ce58e95e27ba
LiuYueKai <liuyuekainihao@163.com>
2017-03-23 10:22:49 +0800

    调整引入顺序以保证基础资源优先引入

--- src/moy.js
+++ src/moy.js
@@ -1,9 +1,9 @@
 import {u} from 'tinper-sparrow/src/index';
+import * as compox from 'compox/src/index';
+import * as compox_util from 'compox-util/src/index';
 import * as neoui from 'tinper-neoui/src/index';
 import {DataTable, u as kero} from 'kero/src/index';
 import * as adapter from 'neoui-kero/src/index';
-import * as compox from 'compox/src/index';
-import * as compox_util from 'compox-util/src/index';
 import * as kero_fetch from 'kero-fetch/src/index';
 import * as neoui_kero_mixin from 'neoui-kero-mixin/src/index';
 
